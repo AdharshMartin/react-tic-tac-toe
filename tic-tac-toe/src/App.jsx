4778f4931a1eb07a1a3aeb99c76ebafe472d554b
Adharsh_Martin <martinadharsh2@gmail.com>
2025-11-12 23:04:24 +0530

    winning

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -18,7 +18,9 @@ const [xNext, setXNext] = useState(true);
 
 function handleClick(i) {
   const nextValue = value.slice();
-  if (nextValue[i]) return;
+
+  if (nextValue[i] || Winningcheck(nextValue)) return;
+  
   if (xNext){
     nextValue[i] = 'X';
   }
@@ -47,6 +49,24 @@ function handleClick(i) {
  )
  }
 
-
+function Winningcheck(value){
+ const lines = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+ ] 
+ for(let i=0;i<lines.length;i++){
+  const [a,b,c] = lines[i];
+  if(value[a] && value[a] === value[b] && value[a] === value[c]){
+    return true;
+  }
+ }
+ return false;
+}
   
 
